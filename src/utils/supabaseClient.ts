0b513f235be540e297e23c784ed4aab98d428730
J7 <j7.dev.gg@outlook.com>
2023-03-19 20:00:21 +0800

    feat: add default supabase key

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = process.env?.REACT_APP_SUPABASE_URL || "";
-const SUPABASE_KEY = process.env?.REACT_APP_SUPABASE_KEY || "";
+const SUPABASE_URL =
+  process.env?.REACT_APP_SUPABASE_URL || "https://supabase.io";
+const SUPABASE_KEY = process.env?.REACT_APP_SUPABASE_KEY || "supabaseKey";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
