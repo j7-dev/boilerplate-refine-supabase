f8f1ef4dd7931afa38c23a086ccd68ead0706310
J7 <j7.dev.gg@outlook.com>
2023-03-24 02:27:40 +0800

    feat: add eslint & antd Theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,69 @@
-import { Refine } from "@refinedev/core";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-import "./assets/scss/style.scss";
+import { Refine } from '@refinedev/core'
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
+import './assets/scss/style.scss'
 
-import { notificationProvider } from "@refinedev/antd";
-import "@refinedev/antd/dist/reset.css";
-
+import { notificationProvider } from '@refinedev/antd'
+import '@refinedev/antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
 import routerBindings, {
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import { dataProvider, liveProvider } from "@refinedev/supabase";
-import { mocksProvider } from "mocks/mocksProvider";
+    UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6'
+import { dataProvider, liveProvider } from '@refinedev/supabase'
+import { mocksProvider } from 'mocks/mocksProvider'
 
-import { useTranslation } from "react-i18next";
-import { BrowserRouter } from "react-router-dom";
-import { supabaseClient } from "utils";
-import authProvider from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import resources from "./resources";
-import AppRoutes from "./Routes";
+import { useTranslation } from 'react-i18next'
+import { BrowserRouter } from 'react-router-dom'
+import { supabaseClient } from 'utils'
+import authProvider from './authProvider'
+import { ColorModeContextProvider } from './contexts/color-mode'
+import resources from './resources'
+import AppRoutes from './Routes'
+import antdThemeConfig from './antdThemeConfig'
 
 function App() {
-  const { t, i18n } = useTranslation();
+    const { t, i18n } = useTranslation()
 
-  const i18nProvider = {
-    translate: (key: string, params: object) => t(key, params),
-    changeLocale: (lang: string) => i18n.changeLanguage(lang),
-    getLocale: () => i18n.language,
-  };
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    }
 
-  const defaultDataProvider =
-    process.env.REACT_APP_MOCK_API === "true"
-      ? mocksProvider
-      : dataProvider(supabaseClient);
+    const defaultDataProvider =
+        process.env.REACT_APP_MOCK_API === 'true'
+            ? mocksProvider
+            : dataProvider(supabaseClient)
 
-  return (
-    <BrowserRouter>
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <Refine
-            dataProvider={{
-              default: defaultDataProvider,
-            }}
-            liveProvider={liveProvider(supabaseClient)}
-            authProvider={authProvider}
-            routerProvider={routerBindings}
-            notificationProvider={notificationProvider}
-            i18nProvider={i18nProvider}
-            resources={resources}
-            options={{
-              syncWithLocation: true,
-              warnWhenUnsavedChanges: true,
-            }}
-          >
-            <AppRoutes />
+    return (
+        <BrowserRouter>
+            <RefineKbarProvider>
+                <ColorModeContextProvider>
+                    <ConfigProvider theme={antdThemeConfig}>
+                        <Refine
+                            dataProvider={{
+                                default: defaultDataProvider,
+                            }}
+                            liveProvider={liveProvider(supabaseClient)}
+                            authProvider={authProvider}
+                            routerProvider={routerBindings}
+                            notificationProvider={notificationProvider}
+                            i18nProvider={i18nProvider}
+                            resources={resources}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                            }}
+                        >
+                            <AppRoutes />
 
-            <RefineKbar />
-            <UnsavedChangesNotifier />
-          </Refine>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+                            <RefineKbar />
+                            <UnsavedChangesNotifier />
+                        </Refine>
+                    </ConfigProvider>
+                </ColorModeContextProvider>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ src/antdThemeConfig.ts
@@ -0,0 +1,32 @@
+const antdThemeConfig = {
+    token: {
+        colorPrimary: '#110964',
+        screenXS: 0,
+        screenXSMax: 374,
+        screenXSMin: 0,
+        screenSM: 375,
+        screenSMMax: 809,
+        screenSMMin: 375,
+        screenMD: 810,
+        screenMDMax: 1079,
+        screenMDMin: 810,
+        screenLG: 1080,
+        screenLGMax: 1279,
+        screenLGMin: 1080,
+        screenXL: 1280,
+        screenXLMax: 1439,
+        screenXLMin: 1280,
+        screenXXL: 1440,
+        screenXXLMin: 1440,
+    },
+    components: {
+        Switch: {
+            colorPrimary: '#14b8a6',
+            colorPrimaryHover: '#0d9488',
+            colorTextQuaternary: '#ff4d4f',
+            colorTextTertiary: '#e11d48',
+        },
+    },
+}
+
+export default antdThemeConfig
